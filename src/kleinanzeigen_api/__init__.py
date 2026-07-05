efb2d82bf449c38a49558b8c71df8d888effbfd9
monkrel <dev.monkrel@gmail.com>
2026-07-05 19:39:35 +0200

    bump version to 0.4.0

--- src/kleinanzeigen_api/__init__.py
+++ src/kleinanzeigen_api/__init__.py
@@ -14,7 +14,7 @@ from .auth import Authenticator, NotLoggedIn
 from .categories import Category, all_categories, find_categories, get_category
 from .client import Conversation, KleinanzeigenAPI, Listing
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = [
     "KleinanzeigenAPI",
     "Listing",
